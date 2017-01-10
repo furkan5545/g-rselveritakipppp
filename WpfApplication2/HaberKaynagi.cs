d43222dfb0db8d09e4a72db2b835a36268a026f8
furkan5545 <furkankucuk554@gmail.com>
2017-01-10 22:53:19 +0300

    gorselveritakipc
    veri takip programı furkan kucuk

---
+++ WpfApplication2/HaberKaynagi.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+namespace WpfApplication2
+{
+    class HaberKaynagi
+    {
+        public class Haber
+        {
+            public string id { get; set; }
+            public string haberurl { get; set; }
+            public string haberbaslik { get; set; }
+            public string haberozet { get; set; }
+
+            public bool yeni { get; set; }
+
+            public string renk { get
+                {
+                    if (yeni == true)
+                    {
+                        return "red";
+                    }
+                    else
+                    {
+                        return "white";
+                    }
+                } }
+        }
+
+        List<Haber> Haberler = new List<Haber>();
+
+
+        public void HaberGuncelle()
+        {
+            XmlDocument xmlkaynak = new XmlDocument();
+            xmlkaynak.Load("http://aa.com.tr/tr/rss/default?cat=guncel");
+
+
+            for(int i = 5; i <xmlkaynak.ChildNodes[0].ChildNodes[0].ChildNodes.Count; i++)
+            {
+                bool yenihaber = true;
+                Haber a = new Haber();
+                a.haberbaslik = xmlkaynak.ChildNodes[0].ChildNodes[0].ChildNodes[i].ChildNodes[2].InnerText;
+                a.id = xmlkaynak.ChildNodes[0].ChildNodes[0].ChildNodes[i].ChildNodes[0].InnerText;
+                a.haberozet = xmlkaynak.ChildNodes[0].ChildNodes[0].ChildNodes[i].ChildNodes[3].InnerText;
+                a.haberurl = xmlkaynak.ChildNodes[0].ChildNodes[0].ChildNodes[i].ChildNodes[1].InnerText;
+                a.yeni = true;
+                for(int j = 0; j < Haberler.Count; j++)
+                {
+                    if (Haberler[j].id == a.id)
+                    {
+                        yenihaber = false;
+                        Haberler[j] = a;
+                        Haberler[j].yeni = false;
+                        break;
+                    }
+                }
+                if (yenihaber == true)
+                {
+                    Haberler.Add(a);
+                }
+
+            }
+
+        }
+
+        public List<Haber> HaberleriVer()
+        {
+            HaberGuncelle();
+            return Haberler;
+        }
+
+    }
+}
---
+++ veritakip.sql
@@ -0,0 +1,69 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 03 Ocak 2017, 04:28:15
+-- Sunucu sürümü: 10.1.16-MariaDB
+-- PHP Sürümü: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `veritakip`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `urunler`
+--
+
+CREATE TABLE `urunler` (
+  `id` int(11) NOT NULL,
+  `urunAd` varchar(100) NOT NULL,
+  `urunAdedi` varchar(100) NOT NULL,
+  `girisTarihi` varchar(100) NOT NULL,
+  `cikisTarihi` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Tablo döküm verisi `urunler`
+--
+
+INSERT INTO `urunler` (`id`, `urunAd`, `urunAdedi`, `girisTarihi`, `cikisTarihi`) 
+(3, 'dsadsa', 'dasdsa', '03.01.2017', '11.12.2016'),
+(6, 'furkan', '10', '03.01.2017', '31.12.2016')
+,
+(11, 'TextBox', 'TextBox', '', ''),
+(12, 'TextBox', 'TextBox', '', '');
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `urunler`
+--
+ALTER TABLE `urunler`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `urunler`
+--
+ALTER TABLE `urunler`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
